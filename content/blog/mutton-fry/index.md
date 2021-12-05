e039911e042051c1370fed97bbaa455f4ac7d0c8
Rebecca <rebapollos@gmail.com>
2021-12-04 19:58:14 -0500

    mutton fry picture added

--- content/blog/mutton-fry/index.md
+++ content/blog/mutton-fry/index.md
@@ -3,3 +3,5 @@ title: Mutton Fry
 date: "2021-12-04"
 description: "Mutton fry recipe."
 ---
+
+![Fish Curry](./Mutton_Fry.jpg)
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
@@ -8,9 +9,18 @@ const Layout = ({ location, title, children }) => {
 
   if (isRootPath) {
     header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
+      <div style={{ display: `flex` }}>
+        <StaticImage
+          layout="fixed"
+          formats={["auto", "webp", "avif"]}
+          src="../images/cookbook.jpg"
+          width={100}
+          alt="Profile picture"
+        />
+        <h1 className="main-heading" style={{ marginLeft: `1rem` }}>
+          <Link to="/">{title}</Link>
+        </h1>
+      </div>
     )
   } else {
     header = (
